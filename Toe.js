bc9a0c32780c1537be1d26f53af9e2d71a0ef331
levi koskan <levikoskan@gmail.com>
2015-12-17 10:44:34 -0600

    completed submit button completed tie outcome

--- Toe.js
+++ Toe.js
@@ -5,15 +5,47 @@ $(function(){
 // }
   var playerMove = 0;
   var count =0;
+  var theGame = false;
 
-  $('.square').on('click',function(){
+// Anytime you click a square element it will change
+  $('.square').on('click', function () {
+      if (playerMove === 1){
+        $(this).empty();
+        $(this).html('<p>o</p>');
+        $(this).removeClass('1')
+        $(this).addClass('2');
+        playerMove = 2;
+
+
+      }  else if (playerMove === 2){
+        $(this).empty();
+        $(this).removeClass('1, 2')
+        playerMove = 0;
+
+      } else {
+        $(this).html('<p>x</p>');
+        $(this).addClass('1');
+        playerMove = 1;
+
+
+      }
+
+      checkWinner(playerMove)
+    });
+
+
+  // button to start a new game. empties all the square elements
+  $('#newGame').on('click', function(){
+    $('.square').empty();
+
+    $('.square').bind('click', function(){
     if (playerMove === 1){
       $(this).empty();
       $(this).html('<p>o</p>');
       $(this).removeClass('1')
       $(this).addClass('2');
       playerMove = 2;
-      count += 1;
+
 
     }  else if (playerMove === 2){
       $(this).empty();
@@ -24,25 +56,73 @@ $(function(){
       $(this).html('<p>x</p>');
       $(this).addClass('1');
       playerMove = 1;
-      count += 1;
+
 
     }
 
     checkWinner(playerMove)
-  })
+  });
+    //remove classes
+    $('div').find('.1').removeClass('1');
+    $('div').find('.2').removeClass('2');
+
+    count = 0;
+    playerMove = 0;
+
+  });
 
 
+  //Submit your move button. no going back, count for tie conditional
+    $('#submit').on('click', function() {
 
-
-
-
-
-
+      $('div').find('.1').unbind('click');
+      $('div').find('.2').unbind('click');
+      count += 1
+      console.log(count);
+      // tie game conditional
+      if (count >= 9 && theGame === false) {
+      alert("Cat's Tail");
+      }
+    });
 
 
 
 
   function checkWinner(player){
+
+    if ($('.top.left').hasClass(player) && $('.top.center').hasClass(player) && $('.top.right').hasClass(player)){
+      alert("Player "+ player +"won by completing the top row.");
+      theGame = true;
+    }
+    if ($('.mid.left').hasClass(player) && $('.mid.center').hasClass(player) && $('.mid.right').hasClass(player)){
+      alert("Player "+ player +"won by completing the middle row.");
+      theGame = true;
+    }
+    if ($('.bottom.left').hasClass(player) && $('.bottom.center').hasClass(player) && $('.bottom.right').hasClass(player)){
+      alert("Player "+ player +"won by completing the bottom row.");
+      theGame = true;
+    }
+    if ($('.top.left').hasClass(player) && $('.mid.left').hasClass(player) && $('.bottom.left').hasClass(player)){
+      alert("Player "+ player +"won by completing the left column");
+      theGame = true;
+    }
+    if ($('.top.center').hasClass(player) && $('.mid.center').hasClass(player) && $('.bottom.center').hasClass(player)){
+      alert("Player "+ player +"won by completing the middle column.");
+      theGame = true;
+    }
+    if ($('.top.right').hasClass(player) && $('.mid.right').hasClass(player) && $('.bottom.right').hasClass(player)){
+      alert("Player "+ player +"won by completing the right column.");
+      theGame = true;
+    }
+    if ($('.top.left').hasClass(player) && $('.mid.center').hasClass(player) && $('.bottom.right').hasClass(player)){
+      alert("Player "+ player +"won by completing the diagonal");
+      theGame = true;
+    }
+    if ($('.top.right').hasClass(player) && $('.mid.center').hasClass(player) && $('.bottom.left').hasClass(player)){
+      alert("Player "+ player +"won by completing the diagonal");
+      theGame = true;
+    }
+
     // var topChecker = $('.topRow').children();
     // var midChecker = $('.midRow').children();
     //   // console.log(midChecker);
@@ -58,32 +138,6 @@ $(function(){
     // console.log($(bottomChecker[index]).hasClass('1') || $(bottomChecker[index]).hasClass('2')
     // );})
 
-
-    if ($('.top.left').hasClass(player) && $('.top.center').hasClass(player) && $('.top.right').hasClass(player)){
-      alert("Player "+ player +"won by completing the top row.");
-    }
-    if ($('.mid.left').hasClass(player) && $('.mid.center').hasClass(player) && $('.mid.right').hasClass(player)){
-      alert("Player "+ player +"won by completing the middle row.");
-    }
-    if ($('.bottom.left').hasClass(player) && $('.bottom.center').hasClass(player) && $('.bottom.right').hasClass(player)){
-      alert("Player "+ player +"won by completing the bottom row.");
-    }
-    if ($('.top.left').hasClass(player) && $('.mid.left').hasClass(player) && $('.bottom.left').hasClass(player)){
-      alert("Player "+ player +"won by completing the left column");
-    }
-    if ($('.top.center').hasClass(player) && $('.mid.center').hasClass(player) && $('.bottom.center').hasClass(player)){
-      alert("Player "+ player +"won by completing the middle column.");
-    }
-    if ($('.top.right').hasClass(player) && $('.mid.right').hasClass(player) && $('.bottom.right').hasClass(player)){
-      alert("Player "+ player +"won by completing the right column.");
-    }
-    if ($('.top.left').hasClass(player) && $('.mid.center').hasClass(player) && $('.bottom.right').hasClass(player)){
-      alert("Player "+ player +"won by completing the diagonal");
-    }
-    if ($('.top.right').hasClass(player) && $('.mid.center').hasClass(player) && $('.bottom.left').hasClass(player)){
-      alert("Player "+ player +"won by completing the diagonal");
-    }
-
     // if ($.each(topChecker, function( index, value){
     //     $(topChecker[index]).hasClass('1')=== true || $(topChecker[index]).hasClass('2') ===true
     // }) && $.each(midChecker, function( index, value ) {
@@ -97,28 +151,9 @@ $(function(){
     // }
     else{
       console.log(count);
+      console.log(theGame);
       return false;
-
     }
   }
-
-
-
-  // $.each(bottomChecker, function( index, value ) {
-  //   $(bottomChecker[index]).hasClass('1') || $(bottomChecker[index]).hasClass('2')
-  //   })
-
-
-
-
-
-
-
-
-function reset(){
-
-}
-
-
 
 });
